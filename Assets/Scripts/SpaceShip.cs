41e3600e9eb7097cc8f00a756e02d0be44010b6c
TanmayMahajan09 <tanmaymahajan912@gmail.com>
2025-02-26 23:51:53 +0530

    Movement Smoothing

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -4,18 +4,35 @@ public class SpaceShip : MonoBehaviour {
 
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Vector3 newDirection;
+    private float interpolationTime = 0.3f;
+    private int movementIterator = 30;
+    private Vector3 moveDirection;
+    private Vector3 oldDirection = Vector3.zero ;
+    private float smoothStep = Mathf.SmoothStep(5, 0, 0.5f);
+    private Rigidbody2D rb;
     private void Awake() {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
-        HandleMovement();
+        HandleMovementnput();
     }
 
-    private void HandleMovement() {
-        
+    private void HandleMovementnput() {
         Vector2 normalisedMovementVector = inputManager.GetNormalisedMovementVector();
-        Vector3 moveDirection = new Vector3 (normalisedMovementVector.x, normalisedMovementVector.y, 0);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        moveDirection = new Vector3 (normalisedMovementVector.x, normalisedMovementVector.y, 0);
+        newDirection = moveDirection;
+        if(newDirection != oldDirection) {
+            interpolationTime = 0.3f;
+            oldDirection = newDirection;
+        }
+        
+
+    }
+
+    private void FixedUpdate() {
+        rb.linearVelocity = Vector2.Lerp(rb.GetPointVelocity(this.transform.position), moveDirection * moveSpeed, interpolationTime * 10 * Time.deltaTime);
+        
     }
 }
